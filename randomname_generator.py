a39931fa50ac50f2b88f82a3d42a7a8e542e2566
SURYADEV-707 <86433380+SURYADEV-707@users.noreply.github.com>
2021-07-29 23:16:49 +0530

    Update randomname_generator.py

--- randomname_generator.py
+++ randomname_generator.py
@@ -1,7 +1,8 @@
 from random import *
+import sys
 from termcolor import colored
 d={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26,'1':27,'2':28,'3':29,'4':30,'5':31,'6':32,'7':33,'8':34,'9':35,'0':36,'!':37,'@':38,'#':39,'$':40,'&':41,'-':42,'_':43,'~':44}
-def myf():
+def myf(l):
     i=0
     rword=""
     while i <l:
@@ -21,8 +22,9 @@ a='''
 print(colored(a,'red'))
 l=int(input("enter the length of password :\t"))
 print()
-print('password   :' , colored(myf(),'green'))
+print('password   :' , colored(myf(l),'green'))
 print()
+sys.exit()
 
 
 
